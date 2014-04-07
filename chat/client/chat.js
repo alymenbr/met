9779a3a0b271f1b90cee6c64c1b3a6398f0f818f
Alysson Anthony <alymenbr@gmail.com>
2014-04-06 23:29:20 -0300

    adiciona login do facebook

--- chat/client/chat.js
+++ chat/client/chat.js
@@ -1,11 +1,23 @@
  
 Router.map( function(){
-	this.route( 'home', {path: '/'} );
+	this.route( 'rooms', {path: '/'} );
 	
 	this.route( 'room', {
 		path: '/rooms/:_id',
 		data: function() { return Rooms.findOne(this.params._id); }} );
 });
+
+Meteor.startup(function() {
+  Template.fb_pic.pic = function() {// helper function to display the pic on the page
+    var userProfile;
+    userProfile = Meteor.user().profile;
+ 
+    if(userProfile) { // logic to handle logged out state
+      return userProfile.picture;
+    }
+  };
+});
+
 
 Template.rooms.helpers({
 	rooms: function(){
@@ -25,17 +37,20 @@ Template.rooms.events = {
 
 Template.room.events = {
 
-	"click button" : function(){
+	"click button" : function(e){
 		var newMessage = document.getElementById("message").value;
-		var newAuthor = document.getElementById("author").value;
+		var newAuthor = Meteor.user().profile.name;
 		var currentId = this._id;
 		addMessage(currentId, newAuthor, newMessage);
+
+		e.preventDefault();
 		return false;
 	}
 };
 
 function addMessage(id, newAuthor, newMessage) {
 		Rooms.update(id, {$push: {messages: {author: newAuthor, text: newMessage}}});
+		Rooms.update(id, {$set : {last_activity : new Date().toDateString() }});
 		clearMessageFields();
 }
 
--- chat/lib/collections.js
+++ chat/lib/collections.js
@@ -1,15 +1,15 @@
 Rooms = new Meteor.Collection('rooms');
 
-if (Meteor.isServer && Rooms.find().count() === 0){
+if (Meteor.isServer && Rooms.find({name: "Meteor Talk"}).count() === 0){
+	Rooms.remove({});
+
 	var rooms = [
 		{name: 'Meteor Talk', members: 3, last_activity: '1 minute ago',
 			messages: [
 				{author: 'Tom', text: 'Hi there Sacha!'},
 				{author: 'Sacha', text: 'Hey Tom, how are you?'},
 				{author: 'Tom', text: 'Good thanks!'}
-			]},
-		{name: 'Dev', members: 2, last_activity: '5 minutes ago'},
-		{name: 'Core', members: 0, last_activity: '3 days ago'}
+			]}
 	];
 
 	rooms.forEach(function (room) {
