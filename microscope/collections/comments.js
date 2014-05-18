84facd64f1f8b82dc1e5fadaa3818b8f9121d52a
Alysson Anthony <alymenbr@gmail.com>
2014-05-18 11:06:28 -0300

    10-3

---
+++ microscope/collections/comments.js
@@ -0,0 +1,26 @@
+Comments = new Meteor.Collection('comments');
+
+Meteor.methods({
+	comment: function(commentAttributes){
+		var user = Meteor.user();
+		var post = Posts.findOne(commentAttributes.postId);
+
+		if(!user)
+			throw new Meteor.Error(401, 'You need to login to make comments');
+		
+		if(!commentAttributes.body)
+			throw new Meteor.Error(422, 'Please add a comment!');
+
+		if(!post)
+			throw new Meteor.Error(422, 'Post not found!');
+
+		comment = _.pick(commentAttributes, 'postId', 'body');
+		comment = _.extend( comment, {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+
+		return Comments.insert(comment);
+	}
+});
