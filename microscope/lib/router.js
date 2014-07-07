ef8eed97359a1eedfc917310cb8cc9a331ffe557
Alysson Anthony <alymenbr@gmail.com>
2014-07-07 12:57:45 -0300

    13-5

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -1,19 +1,19 @@
 Router.configure({
   layoutTemplate: 'layout',
   loadingTemplate: 'loading',
-  waitOn: function() {
-    return [Meteor.subscribe('notifications')];
+  waitOn: function() { 
+    return [Meteor.subscribe('notifications')]
   }
 });
 
 PostsListController = RouteController.extend({
   template: 'postsList',
-  increment: 5,
-  limit: function() {
-    return parseInt(this.params.postsLimit, 10) || this.increment;
+  increment: 5, 
+  limit: function() { 
+    return parseInt(this.params.postsLimit) || this.increment; 
   },
   findOptions: function() {
-    return {sort: {submitted: -1}, limit: this.limit()};
+    return {sort: this.sort, limit: this.limit()};
   },
   waitOn: function() {
     return Meteor.subscribe('posts', this.findOptions());
@@ -23,42 +23,65 @@ PostsListController = RouteController.extend({
   },
   data: function() {
     var hasMore = this.posts().count() === this.limit();
-    var nextPath = this.route.path({postsLimit: this.limit() + this.increment});
     return {
       posts: this.posts(),
-      nextPath: hasMore ? nextPath : null
+      nextPath: hasMore ? this.nextPath() : null
     };
   }
 });
 
+NewPostsListController = PostsListController.extend({
+  sort: {submitted: -1, _id: -1},
+  nextPath: function() {
+    return Router.routes.newPosts.path({postsLimit: this.limit() + this.increment})
+  }
+});
+
+BestPostsListController = PostsListController.extend({
+  sort: {votes: -1, submitted: -1, _id: -1},
+  nextPath: function() {
+    return Router.routes.bestPosts.path({postsLimit: this.limit() + this.increment})
+  }
+});
+
 Router.map(function() {
+  this.route('home', {
+    path: '/',
+    controller: NewPostsListController
+  });
+  
+  this.route('newPosts', {
+    path: '/new/:postsLimit?',
+    controller: NewPostsListController
+  });
+  
+  this.route('bestPosts', {
+    path: '/best/:postsLimit?',
+    controller: BestPostsListController
+  });
+  
   this.route('postPage', {
     path: '/posts/:_id',
     waitOn: function() {
       return [
         Meteor.subscribe('singlePost', this.params._id),
         Meteor.subscribe('comments', this.params._id)
-        ];
+      ];
     },
     data: function() { return Posts.findOne(this.params._id); }
   });
 
   this.route('postEdit', {
     path: '/posts/:_id/edit',
-    waitOn: function() {
-      return Meteor.subscribe('comments', this.params._id);
+    waitOn: function() { 
+      return Meteor.subscribe('singlePost', this.params._id);
     },
     data: function() { return Posts.findOne(this.params._id); }
   });
   
   this.route('postSubmit', {
     path: '/submit',
-    disableProgress: true
-  });
-  
-  this.route('postsList', {
-    path: '/:postsLimit?',
-    controller: PostsListController
+    progress: {enabled: false}
   });
 });
 
@@ -72,8 +95,8 @@ var requireLogin = function(pause) {
     
     pause();
   }
-};
+}
 
 Router.onBeforeAction('loading');
 Router.onBeforeAction(requireLogin, {only: 'postSubmit'});
-Router.onBeforeAction(function() { clearErrors(); });
+Router.onBeforeAction(function() { clearErrors() });
